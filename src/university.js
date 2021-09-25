da358ad3f681b19d569bf5e5c5850dfce3d513b1
Jubair-Ahmed-Khan <jubair.mbstu@gmail.com>
2021-09-26 01:43:07 +0600

    first commit

---
+++ src/university.js
@@ -0,0 +1,130 @@
+const university = [
+    {
+        img: "images/du.jpg",
+        name: "Dhaka University",
+        established: "1921",
+        address: "Nilkhet Rd, Dhaka 1000",
+        area: "600 acres",
+        formFillupFee: 900
+    },
+    {
+        img: "images/cu.jpg",
+        name: "Chittagong University",
+        established: "1966",
+        address: "Chittagong University Rd, 4331",
+        area: "2110 acres",
+        formFillupFee: 750
+    },
+    {
+        img: "images/ru.jpg",
+        name: "Rajshahi University",
+        established: "1953",
+        address: "Administration Building 1, Rajshah",
+        area: "753 acres",
+        formFillupFee: 800
+    },
+    {
+        img: "images/ku.jpg",
+        name: "Khulna University",
+        established: "1991",
+        address: "Sher-E-Bangla Rd, Khulna 9208",
+        area: "106 acres",
+        formFillupFee: 650
+    },
+    {
+        img: "images/sau.jpg",
+        name: "Sylhet Agricultural University",
+        established: "2006",
+        address: "Alurtol Road, Sylhet",
+        area: "49 acres",
+        formFillupFee: 700
+    },
+    {
+        img: "images/bu.jpg",
+        name: "Barishal University",
+        established: "2011",
+        address: "Kornokathi, Barishal 8200",
+        area: "50 acres",
+        formFillupFee: 630
+    },
+    {
+        img: "images/buet.jpg",
+        name: "Bangladesh University of Engineering and Technology",
+        established: "1971",
+        address: "Nilkhet, Dhaka",
+        area: "91.37 acres",
+        formFillupFee: 1000
+    },
+    {
+        img: "images/kuet.jpg",
+        name: "Khulna University of Engineering and Technology",
+        established: "1967",
+        address: "WG22+6WH, Khulna 9203",
+        area: "117.35 acres",
+        formFillupFee: 820
+    },
+    {
+        img: "images/ruet.jpg",
+        name: "Rajshahi University of Engineering and Technology",
+        established: "1964",
+        address: "6204 Rajshahi - Dhaka Hwy, Rajshahi",
+        area: "152 acres",
+        formFillupFee: 780
+    },
+    {
+        img: "images/cuet.jpg",
+        name: "Chittagong University of Engineering and Technology",
+        established: "1968",
+        address: "Kaptai, Chattogram 4349",
+        area: "188 acres",
+        formFillupFee: 550
+    },
+    {
+        img: "images/ju.jpg",
+        name: "Jahangirnagar University",
+        established: "1970",
+        address: "Kalabagan Rd, Savar Union 1342",
+        area: "697.56 acres",
+        formFillupFee: 950
+    },
+    {
+        img: "images/jnu.jpg",
+        name: "Jagannath University",
+        established: "2005",
+        address: "9-10 Chittaranjan Ave, Dhaka 1100",
+        area: "7.5 acres",
+        formFillupFee: 640
+    },
+    {
+        img: "images/mbstu.jpg",
+        name: "Mawlana Bhashani Science and Technology University",
+        established: "1999",
+        address: "Tangail, Santosh 1902",
+        area: "70.69 acres",
+        formFillupFee: 800
+    },
+    {
+        img: "images/mbstu.jpg",
+        name: "Mawlana Bhashani Science and Technology University",
+        established: "1999",
+        address: "Tangail, Santosh 1902",
+        area: "70.69 acres",
+        formFillupFee: 800
+    },
+    {
+        img: "images/pust.jpg",
+        name: "Pabna University of Science and Technology",
+        established: "2008",
+        address: "Rajapur, Pabna 6600",
+        area: "30 acres",
+        formFillupFee: 720
+    },
+    {
+        img: "images/pstu.jpg",
+        name: "Patuakhali Science and Technology University",
+        established: "2000",
+        address: "Dhaka - Patuakhali Highway, 8602",
+        area: "89.97 acres",
+        formFillupFee: 670
+    }
+]
